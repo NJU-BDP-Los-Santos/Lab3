bdc187075a9a62c104a33eaac3d596982b903564
HELLORPG <hellorpg2017@gmail.com>
2020-04-28 09:38:25 -0700

    [add] 增加了自定义类ProductOrder

--- Code/src/ReduceSide/Main.java
+++ Code/src/ReduceSide/Main.java
@@ -1,11 +1,16 @@
 package ReduceSide;
 
 
+import ReduceSide.ProductOrder;
+
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
 //import org.apache.hadoop.mapred.lib.HashPartitioner;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -16,30 +21,34 @@ import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 import org.apache.hadoop.io.Text;
+import org.mockito.internal.matchers.Null;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main // 主函数（类）
+{
+    // 需要自定义类型进行表示
+
     public static void main(String[] args) throws Exception
     {
         try {
             Configuration conf = new Configuration();
             String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
             if (otherArgs.length != 2) {
-                System.err.println("Usage: <in> <out>");
+                System.err.println("Please Use the command: <input path> <output path>");
                 System.exit(2);
             }
 
-            Job job = new Job(conf, "Inverted-Index-Basic");
+            Job job = new Job(conf, "Reduce-Side-Join");
             job.setJarByClass(Main.class);
-            job.setMapperClass(TokenizerMapper.class);
+            job.setMapperClass(ReaderMapper.class);
             job.setReducerClass(MergeReducer.class);
             job.setCombinerClass(CombinerSameWordDoc.class);
             job.setPartitionerClass(DividePartitioner.class);
-            job.setNumReduceTasks(4);
+            job.setNumReduceTasks(1);
             job.setOutputKeyClass(Text.class);
             job.setOutputValueClass(IntWritable.class);
 //            job.setMapOutputKeyClass(Text.class);
@@ -55,29 +64,49 @@ public class Main {
         }
     }
 
-    public static class TokenizerMapper extends Mapper<LongWritable,Text,Text,IntWritable>
+    public static class ReaderMapper extends Mapper<LongWritable,Text,ProductOrder, NullWritable>
+        // 用于读取文件中的信息，并且组织成为自定义的类型
     {
         @Override
         protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException
         {
 
             FileSplit fileSplit = (FileSplit)context.getInputSplit();
-            String fileName = fileSplit.getPath().getName(); // 获得当前文件的文件名
+            String fileName = fileSplit.getPath().getName();    // 获得当前文件的文件名
+            String fileKind = fileName.split(("\\."))[0];       // 获得当前文件的种类：product or order
 
-            String[] point_divide = fileName.split("\\.");
-            String docName = point_divide[0] + point_divide[1]; // 按照输出样例，消除了小说明中的.
-            Text word = new Text();
-//            IntWritable count;
-//            HashMap<Text, Integer> hashMap = new HashMap<>();
             StringTokenizer tokens = new StringTokenizer(value.toString());
+//            System.out.println(value.toString());
+            String[] data = new String[4];
 
+            int i = 0;
             while(tokens.hasMoreTokens())
             {
-                word.set(tokens.nextToken());
-                Text word_filename = new Text(word + "#" + docName); // 创建复合键
-                context.write(word_filename, new IntWritable(1));
-//                context.write(word, new Text(fileName));
+                data[i] = tokens.nextToken().toString();
+                ++i;
             }
+
+            // 这里对i的情况进行分类讨论
+            ProductOrder po;
+            if (i == 3)
+            {
+                po = new ProductOrder(Integer.parseInt(data[0]), data[1], Integer.parseInt(data[2]));
+            }
+            else
+            {
+                po = new ProductOrder(Integer.parseInt(data[0]), Integer.parseInt(data[1]), data[2], Integer.parseInt(data[3]));
+            }
+
+            context.write(po, NullWritable.get());
+
+//
+//            while(tokens.hasMoreTokens())
+//            {
+//                word.set(tokens.nextToken());
+//                Text word_filename = new Text(word + "#" + docName); // 创建复合键
+////                context.write(word_filename, new IntWritable(1));
+////                context.write(word, new Text(fileName));
+//            }
         }
     }
     public static class CombinerSameWordDoc extends Reducer<Text, IntWritable, Text, IntWritable>
@@ -142,7 +171,7 @@ public class Main {
             if (!t.equals(t_prev) && t_prev != null && !t_prev.equals(""))
             {
                 double average = words_sum / doc_sum;
-                context.write(new Text(t_prev + "\t" + doubleTransform(average) + ","), new Text(output_));
+//                context.write(new Text(t_prev + "\t" + doubleTransform(average) + ","), new Text(output_));
                 output_ = "";
                 words_sum = 0.0;
                 doc_sum = 0.0;
@@ -156,7 +185,7 @@ public class Main {
         protected void cleanup(Context context) throws IOException, InterruptedException
         {
             double average = words_sum / doc_sum;
-            context.write(new Text(t_prev + "\t" + doubleTransform(average) + ","), new Text(output_));
+//            context.write(new Text(t_prev + "\t" + doubleTransform(average) + ","), new Text(output_));
 //            context.write(new Text(t_prev + ","), new Text(output_));
         }
     }
